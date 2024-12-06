a8678b5bcfb752450c2c72d347e6d23c0ed0882c
Marlo Pato <Marlo_pato123@hotmail.com>
2024-12-06 11:46:58 +0100

    added userlist with basic css

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,10 @@
-import { useState } from "react";
 import "./App.css";
+import UserList from "./components/UserList";
 
 function App() {
-  const [count, setCount] = useState(0);
-
   return (
     <>
-      <h1>marlos branch</h1>
+      <UserList />
     </>
   );
 }
